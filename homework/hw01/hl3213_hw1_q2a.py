5150f1b8d2e200a383f339a9a1ac99196ea9062a
hurmin <hl3213@nyu.edu>
2019-02-06 11:48:20 -0500

    edited

--- homework/hw01/hl3213_hw1_q2a.py
+++ homework/hw01/hl3213_hw1_q2a.py
@@ -3,9 +3,10 @@
 #post: arr
 #shifts numbers in lst k indicies to the left
 def shift(lst, k):
-    for x in range(k):
+    while k > 0:
         stor = lst[0]
         for i in range(len(lst) - 1):
             lst[i] = lst[i + 1]
         lst[len(lst) - 1] = stor
+        k -= 1
     return lst
