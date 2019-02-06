5feac7a8c25fe33647f7e0b3715244853198ecb9
hurmin <hl3213@nyu.edu>
2019-02-06 11:27:06 -0500

    comments

--- homework/hw01/hl3213_hw1_q6.py
+++ homework/hw01/hl3213_hw1_q6.py
@@ -1,4 +1,8 @@
 class Vector:
+    #__init__
+    #pre: Vector, int/list
+    #post: 
+    #initializes a Vector with coordinates of d
     def __init__(self, d):
         if isinstance(d, int):
             self.coords = [0]*d
@@ -22,7 +26,12 @@ class Vector:
             result[j] = self[j] + other[j]
         return result
 
-    #========NEW METHODS=======
+    #=============NEW METHODS============
+
+    #__sub__
+    #pre: Vector, Vector
+    #post: Vector
+    #gets difference between two Vector coordinates
     def __sub__(self, other):
         if (len(self) != len(other)):
             raise ValueError('dimensions must agree')
@@ -31,12 +40,20 @@ class Vector:
             result[k] = other[k] - self[k]
         return result
     
+    #__neg__
+    #pre: Vector
+    #post: Vector
+    #negates the coordinates of a Vector
     def __neg__(self):
         result = Vector(len(self))
         for l in range(len(self)):
             result[l] = self[l] * -1
         return result
 
+    #__mul__
+    #pre: Vector, Vector/int
+    #post: Vector
+    #returns the scalar/dot product of the inputs
     def __mul__(self, other):
         if isinstance(other, int):
             result = Vector(len(self))
@@ -52,12 +69,17 @@ class Vector:
                     result += self[x] * other[x]
                 return result
     
+    #__rmul__
+    #pre: Vector, int
+    #post: Vector
+    #returns the scalar product of the inputs
     def __rmul__(self, other):
         result = Vector(len(self))
         for x in range(len(self)):
             result[x] = self[x] * other
         return result
-    #==========================
+
+    #====================================
     def __eq__(self, other):
         return self.coords == other.coords
     
