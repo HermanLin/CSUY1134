c844ac2b9adc174803402598445081fd6cbef5bf
hurmin <hl3213@nyu.edu>
2019-02-06 10:38:47 -0500

    hw1q5

--- homework/hw01/hl3213_hw1_q5.py
+++ homework/hw01/hl3213_hw1_q5.py
@@ -3,15 +3,10 @@
 #post: arr
 #returns a generator that contains the first n elements in the Fibonacci sequence
 def fibs(n):
-    curr, prev, stor = 1, 0, 0
-    ret = []
-    if n >= 1:
-        ret.append(1)
-        for i in range(n - 1):
-            ret.append(curr + prev)
-            stor = curr
-            curr += prev       
-            prev = stor 
-    else:
-        return ret
-    return ret
+    curr, prev, stor = 1, 1, 0
+    while n > 0:
+        yield prev
+        stor = curr
+        curr += prev       
+        prev = stor 
+        n -= 1
