373e62a4c2a1a6be1ffd558e886b46faaba1cb2f
hurmin <hl3213@nyu.edu>
2019-02-20 01:06:18 -0500

    hw2q6

--- homework/hw02/hl3213_hw2_q6.py
+++ homework/hw02/hl3213_hw2_q6.py
@@ -1,9 +1,20 @@
 def two_sum(srt_lst, target):
-    lstLen = len(srt_lst)
-    addend1 = 0
-    addend2 = lstLen - 1
-    i = 0
-    j = 0
+    i, j = 0, len(srt_lst) - 1
 
-    while addend1 < addend2 and addend2 != lstLen - 1:
-        
+    while i < j:
+        total = srt_lst[i] + srt_lst[j]
+        #print("Total: ", total)
+        if total < target:
+            i += 1
+        elif total > target:
+            j -= 1
+        elif total == target:
+            return (i, j)
+        else:
+            return None
+
+def main():
+    srt_lst = [-2,7,11,15,20,21]
+    two_sum(srt_lst, 22)
+
+#main()
