5e02442b06ca80e1cbb5f3b3ff96cca6da230e7b
hurmin <hl3213@nyu.edu>
2019-02-19 23:57:57 -0500

    hw5

--- homework/hw02/hl3213_hw2_q5.py
+++ homework/hw02/hl3213_hw2_q5.py
@@ -1,2 +1,23 @@
 def split_parity(lst):
-    
+    l, r = 0, len(lst) - 1
+    stor = 0
+    #print(lst)
+
+    while l <= r:
+        #print(str(l) + ", " + str(r)
+        if lst[l] % 2 == 0:
+            if lst[r] % 2 == 1:
+                stor = lst[l]
+                lst[l] = lst[r]
+                lst[r] = stor
+            else:
+                r -= 1
+        else:
+            l += 1
+    return lst
+
+def main():
+    split_parity([1,2,3,4])
+    split_parity([1,5,2,8,4,7,6,3,9])
+
+#main()
