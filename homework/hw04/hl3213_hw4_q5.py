579a2ac9cd6faafc2ab5b3176257b22b69aa4ee9
hurmin <hl3213@nyu.edu>
2019-03-10 17:17:16 -0400

    hw4q5 dnf

---
+++ homework/hw04/hl3213_hw4_q5.py
@@ -0,0 +1,11 @@
+def count_lowercase(s, low, high):
+    ctr = 0
+    if low == high:
+        if s[low].islower():
+            ctr += 1
+    else:
+        count_lowercase(s, low + 1, high)
+
+
+s = "LZibRfVnhi"
+print(count_lowercase(s, 3, 3))
