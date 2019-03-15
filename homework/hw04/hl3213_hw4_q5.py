c647d5e9cc13db2b9f6969d0f6363be5f9afaeb6
hurmin <hl3213@nyu.edu>
2019-03-15 08:17:17 -0400

    hw4q5

--- homework/hw04/hl3213_hw4_q5.py
+++ homework/hw04/hl3213_hw4_q5.py
@@ -1,11 +1,35 @@
 def count_lowercase(s, low, high):
-    ctr = 0
+    count = 0
     if low == high:
         if s[low].islower():
-            ctr += 1
+            return 1
     else:
-        return ctr + count_lowercase(s, low + 1, high)
+        count += count_lowercase(s, low + 1, high)
+        if s[low].islower():
+            count += 1
+    return count
 
+#s = "zxCKpljwuO" #7
+#s1 = "zxCKpljwuo" #8
+#print(count_lowercase(s, 0, 9))
 
-s = "LZibRfVnhi"
-print(count_lowercase(s, 3, 3))
+def is_number_of_lowercase_even(s, low, high):
+    evenOdd = True
+    if low == high:
+        if s[low].islower():
+            return False
+        return True
+    else:
+        evenOdd = is_number_of_lowercase_even(s, low + 1, high)
+        if s[low].islower():
+            if evenOdd == False:
+                evenOdd = True
+            else:
+                evenOdd = False
+    return evenOdd
+        
+
+#False
+#print(is_number_of_lowercase_even(s, 0, 9))
+#True
+#print(is_number_of_lowercase_even(s1, 0, 9))
